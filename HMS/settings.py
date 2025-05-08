803ceec32eb052a3b7d24dd705dbb805b85eee34
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2025-05-08 18:42:29 +0530

    deploy

--- HMS/settings.py
+++ HMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#+w07wb0m_07u$iy)rrdprxgh00dj(m1qyi75z1%0a*d8o8_q(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
